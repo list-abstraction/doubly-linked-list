e92310229353b9a90a2d2dc32e82379cddc0497e
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-04-28 23:04:21 +0200

    :mag: test: Make tests run.

--- test/src/DoublyLinkedList.js
+++ test/src/DoublyLinkedList.js
@@ -1,31 +1,31 @@
 import test from 'ava';
 import * as dll from '../../src';
 
-test( "DoublyLinkedList" , function ( assert ) {
+test( "DoublyLinkedList" , t => {
 
-	var list = new dll.DoublyLinkedList( ) ;
+	const list = new dll.DoublyLinkedList( ) ;
 
-	var a = list.push( 1 ) ;
-	var b = list.push( 2 ) ;
-	var c = list.push( 3 ) ;
-	var d = list.push( 4 ) ;
+	const a = list.push( 1 ) ;
+	const b = list.push( 2 ) ;
+	const c = list.push( 3 ) ;
+	const d = list.push( 4 ) ;
 
-	assert.equal( list.length , 4 ) ;
+	t.deepEqual( list.length , 4 ) ;
 
-	assert.t.deepEqual( list.erase( b ) , c ) ;
+	t.deepEqual( list.erase( b ) , c ) ;
 
-	assert.equal( list.length , 3 ) ;
+	t.deepEqual( list.length , 3 ) ;
 
-	assert.t.deepEqual( list.rerase( c ) , a ) ;
+	t.deepEqual( list.rerase( c ) , a ) ;
 
-	assert.equal( list.length , 2 ) ;
+	t.deepEqual( list.length , 2 ) ;
 
-	assert.t.deepEqual( list.erase( a ) , d ) ;
+	t.deepEqual( list.erase( a ) , d ) ;
 
-	assert.equal( list.length , 1 ) ;
+	t.deepEqual( list.length , 1 ) ;
 
-	assert.t.deepEqual( list.erase( d ) , list.end( ) ) ;
+	t.deepEqual( list.erase( d ) , list.end( ) ) ;
 
-	assert.equal( list.length , 0 ) ;
+	t.deepEqual( list.length , 0 ) ;
 
 } ) ;
--- test/src/list-spec.js
+++ test/src/list-spec.js
@@ -1,6 +1,5 @@
 import test from 'ava';
 import * as dll from '../../src';
 
-import spec from "@aureooms/js-list-spec" ;
-spec.test( "DoublyLinkedList" , dll.DoublyLinkedList ) ;
-
+import * as spec from "@aureooms/js-list-spec" ;
+spec.test( test , "DoublyLinkedList" , dll.DoublyLinkedList ) ;
